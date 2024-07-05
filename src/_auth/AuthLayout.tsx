7054022a750fb4924a5a15f010ea52574efe3d40
faizanhasan <mdfaizanalam367@gmail.com>
2024-07-05 22:43:13 +0530

    update AutLayout and add useUserContext

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,25 +1,27 @@
+import { useUserContext } from '@/context/AuthContext';
+
 import { Outlet, Navigate } from 'react-router-dom';
 
-const AuthLayout = () => {
-  const isAuthenticated = false;
+export default function AuthLayout() {
+  const { isAuthenticated } = useUserContext();
 
   return (
     <>
-    {
-      isAuthenticated ? (
+      {isAuthenticated ? (
         <Navigate to='/' />
-      ): (
-            <>
-              <section className='flex flex-1 justify-center items-center flex-col py-10'>
-                <Outlet />
-              </section>
+      ) : (
+        <>
+          <section className='flex flex-1 justify-center items-center flex-col py-10'>
+            <Outlet />
+          </section>
 
-              <img src="/assets/images/side-img.svg" alt="Side Image" className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat' />
-            </>
-      )
-    }
+          <img
+            src='/assets/images/side-img.svg'
+            alt='Side Image'
+            className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'
+          />
+        </>
+      )}
     </>
-  )
+  );
 }
-
-export default AuthLayout
